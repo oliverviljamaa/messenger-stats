c87339ac7d614e90368299be812e60553ba4faa4
Oliver Viljamaa <oliver.viljamaa@gmail.com>
2020-03-31 16:17:48 +0300

    Updates

---
+++ precache-manifest.e2c2a00d4eb21473ac151502af754129.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93e79f14503eca31fa3ac4ec17b5dbcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e907885dc7e281a1300",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "86029be0944705cf5d59",
+    "url": "/static/js/2.2f7e3363.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.2f7e3363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e907885dc7e281a1300",
+    "url": "/static/js/main.56477228.chunk.js"
+  },
+  {
+    "revision": "b353998baaa1c666e374",
+    "url": "/static/js/runtime-main.f64d3a5d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
