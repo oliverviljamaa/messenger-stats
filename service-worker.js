4705f8d38de8ee652aef0626657ce0238f73a81e
Oliver Viljamaa <oliver.viljamaa@gmail.com>
2020-03-31 16:41:43 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2aa65473496c5e852912c43c9e093eed.js"
+  "./precache-manifest.4a9ac6cd8d850f00b2865a4e613d5232.js"
 );
 
 self.addEventListener('message', (event) => {
