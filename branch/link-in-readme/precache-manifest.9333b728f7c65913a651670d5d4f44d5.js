cf95bb5b5ce9791ade6b85ffbded6720a8bef70f
Oliver Viljamaa <oliver.viljamaa@gmail.com>
2020-04-01 07:58:07 +0000

    Updates

---
+++ branch/link-in-readme/precache-manifest.9333b728f7c65913a651670d5d4f44d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cdc66f19ef9f19704865213c31a2272",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9f08c2f5d43084ade3a3",
+    "url": "./static/css/2.341af901.chunk.css"
+  },
+  {
+    "revision": "bbff0b9d7c41bfffd140",
+    "url": "./static/css/main.88824ec2.chunk.css"
+  },
+  {
+    "revision": "9f08c2f5d43084ade3a3",
+    "url": "./static/js/2.069435e2.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "./static/js/2.069435e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbff0b9d7c41bfffd140",
+    "url": "./static/js/main.45a0ab86.chunk.js"
+  },
+  {
+    "revision": "e6dd8b9cf16fe579c385",
+    "url": "./static/js/runtime-main.9238e184.js"
+  }
+]);
