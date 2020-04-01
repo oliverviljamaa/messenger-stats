20700e81fc397443d1da167785bf19b02bd79682
Oliver Viljamaa <oliver.viljamaa@gmail.com>
2020-04-01 18:15:08 +0300

    Add stateful hot reloading

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,28 @@
-const { override, fixBabelImports } = require('customize-cra'); // eslint-disable-line @typescript-eslint/no-var-requires
+/* eslint-disable fp/no-mutation, no-param-reassign, @typescript-eslint/no-var-requires, @typescript-eslint/explicit-function-return-type */
+const compose = require('lodash/fp/compose');
+const { fixBabelImports } = require('customize-cra');
+const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 
-module.exports = override(
-  fixBabelImports('import', {
+module.exports = (config, env) => {
+  const override = compose(configureAntToImportStylesForComponents, configureReactHotLoader);
+  return override(config, env);
+};
+
+function configureAntToImportStylesForComponents(config, env) {
+  return fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
     style: 'css',
-  }),
-);
+  })(config, env);
+}
+
+function configureReactHotLoader(config, env) {
+  config = rewireReactHotLoader(config, env);
+
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    'react-dom': '@hot-loader/react-dom',
+  };
+
+  return config;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { hot } from 'react-hot-loader'; // eslint-disable-line import/no-extraneous-dependencies
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const HotApp = hot(module)(App);
+ReactDOM.render(<HotApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
